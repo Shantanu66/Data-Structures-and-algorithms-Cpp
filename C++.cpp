9368a37c0545639c804ea733efef2d3c1787ef73
Shantanu66 <shantanur66@gmail.com>
2021-09-17 22:46:43 +0530

    First

---
+++ C++.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+///Cpp Version check
+int main()
+{
+    if (__cplusplus == 201703L) cout << "C++17\n";
+    else if (__cplusplus == 201402L) cout << "C++14\n";
+    else if (__cplusplus == 201103L) cout << "C++11\n";
+    else if (__cplusplus == 199711L) cout << "C++98\n";
+    else cout << "pre-standard C++\n";
+}
+
+///Template
+/*template <typename T>
+T myMax(T x, T y)
+{
+   return (x > y)? x: y;
+}
+
+int main()
+{
+  cout << myMax<int>(3, 7) << endl;
+  cout << myMax<double>(3.0, 7.0) << endl;
+  cout << myMax<char>('g', 'e') << endl;
+
+  return 0;
+}*/
