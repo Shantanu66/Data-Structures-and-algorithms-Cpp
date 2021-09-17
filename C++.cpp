669bbf8e6eedf152a54827b184c3f54cf121465b
Shantanu66 <shantanur66@gmail.com>
2021-09-17 22:55:42 +0530

    Template codes

--- C++.cpp
+++ C++.cpp
@@ -12,7 +12,7 @@ int main()
 }
 
 ///Template
-/*template <typename T>
+template <typename T>
 T myMax(T x, T y)
 {
    return (x > y)? x: y;
@@ -25,4 +25,103 @@ int main()
   cout << myMax<char>('g', 'e') << endl;
 
   return 0;
-}*/
+}
+template<typename T>      //for all DT a single function use template
+T mymax(T a,T b)
+{
+    return (a>b)?a:b;
+}
+int main()
+{
+    cout<<mymax<int>(5,6)<<endl;
+    cout<<mymax<char>('a','b')<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int largest=a[0];
+    //int largest=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            largest=a[i];
+        }
+    }
+    cout<<"Largest element:"<<largest<<endl;
+return 0;
+}
+int main()
+{
+   int a,b;
+   cout<<"Enter elements:"<<endl;
+   cin>>a>>b;
+   cout<<"Before Swapping:"<<endl;
+   cout<<"a:"<<a<<endl;
+   cout<<"b:"<<b<<endl;
+   int temp=a;
+   a=b;
+   b=temp;
+   /*a=a^b;
+   b=a^b;
+   a=a^b;*/
+   swap(a,b);
+   cout<<"After Swapping:"<<endl;
+   cout<<"a:"<<a<<endl;
+   cout<<"b:"<<b<<endl;
+
+   return 0;
+}
+int main()
+{
+    cout<<"Enter no. of elements:"<<endl;
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+
+        cin>>a[i];
+        cout<<"Elements before:"<<a[i]<<endl;
+
+
+    }
+    for(int i=n-1;i>=0;i--)
+    {
+
+        cout<<"Elements after:"<<a[i]<<endl;
+    }
+    return 0;
+}
+
+template <class T, int max>
+int arrMin(T arr[], int n)
+{
+   int m = max;
+   for (int i = 0; i < n; i++)
+      if (arr[i] < m)
+         m = arr[i];
+
+   return m;
+}
+
+int main()
+{
+   int arr1[]  = {10, 20, 15, 12};
+   int n1 = sizeof(arr1)/sizeof(arr1[0]);
+
+   char arr2[] = {1, 2, 3};
+   int n2 = sizeof(arr2)/sizeof(arr2[0]);
+
+   // Second template parameter to arrMin must be a constant
+   cout << arrMin<int, 10000>(arr1, n1) << endl;
+   cout << arrMin<char, 256>(arr2, n2);
+   return 0;
+}
