4172cd8e2f339bd70904e26950a35e196ad5ca8b
Maxman <shantanur66@gmail.com>
2021-10-31 23:53:34 +0530

    update

--- C++.cpp
+++ C++.cpp
@@ -1380,3 +1380,18 @@ int main()
     int i=s;
     int j=mid+1;
     int k=s;
+    while(i<=mid && j<=e)
+    {
+        if(x[i]<y[j])
+        {
+            a[k]=x[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            a[k]=y[j];
+            k++;
+            j++;
+        }
+    }
