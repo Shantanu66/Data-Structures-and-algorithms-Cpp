b7c4d7e4d04b70697ef1235b48bf743d602ff09d
Maxman <shantanur66@gmail.com>
2021-12-07 23:47:20 +0530

    update

--- C++.cpp
+++ C++.cpp
@@ -2560,3 +2560,30 @@ int main()
     cout<<s1.IsEmpty()<<endl;
     return 0;
 }*/
+///Inbuilt Stack
+/*int main(){
+   stack<int> s;
+   s.push(1);
+   s.push(2);
+   s.push(3);
+   s.push(4);
+   s.push(5);
+   s.push(6);
+
+   cout<<s.top()<<endl;
+   s.pop();
+   cout<<s.top()<<endl;
+   s.pop();
+
+   cout<<s.size()<<endl;
+   cout<<s.empty()<<endl;
+
+   while(!s.empty())
+   {
+    cout<<s.top()<<endl;
+    s.pop();
+   }
+   cout<<s.size()<<endl;
+
+   return 0;
+}*/
