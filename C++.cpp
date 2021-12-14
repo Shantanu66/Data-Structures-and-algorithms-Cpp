60f89e57ca5bea10ccdafc794786ddaaf3350afb
Maxman <shantanur66@gmail.com>
2021-12-14 23:26:44 +0530

    update

--- C++.cpp
+++ C++.cpp
@@ -2744,3 +2744,31 @@ public:
         }
     }
 };
+
+int main()
+{
+
+ Queue<int> q(5);
+ q.push(10);
+ q.push(20);
+ q.push(30);
+ q.push(40);
+ q.push(50);
+ q.push(60);
+ q.push(70);
+ cout<<q.front()<<endl;
+ q.pop();
+ q.pop();
+ q.pop();
+ cout<<q.front()<<endl;
+ cout<<q.Size()<<endl;
+ cout<<q.IsEmpty()<<endl;
+ q.push(60);
+ q.push(70);
+
+ q.pop();
+ q.pop();
+ cout<<q.front()<<endl;
+ cout<<q.Size()<<endl;
+ return 0;
+}*/
