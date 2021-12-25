b05b239d0ba7eb8200cb1c53dfccb4f45ea9d2e4
Maxman <shantanur66@gmail.com>
2021-12-25 23:42:04 +0530

    Tress beginnnig

--- C++.cpp
+++ C++.cpp
@@ -2935,3 +2935,25 @@ int main()
     }
     return 0;
 }*/
+
+///TREES
+///GENERIC TREES
+/*template<typename T>
+class TreeNode
+{
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    TreeNode(T data)
+    {
+        this->data=data;
+    }
+    ~TreeNode()
+    {
+        //cout<<"Tree Deleted!"<<endl;
+        for(int i=0;i<children.size();i++)
+        {
+            delete children[i];
+        }
+    }
+};
